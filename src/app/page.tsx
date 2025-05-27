96ed1ce01c146c04836c83736083073ef6687672
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-27 11:21:37 +0330

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import Calendar from "@/components/calender";
+import CalendarPage from "@/components/calender";
 import Dashboard from "@/components/dashboard";
 import { db } from "@/db";
 import { Task } from "@/generated/prisma";
+import { Check, Star } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
   const tasks: Task[] = await db.task.findMany();
+
+  const totalCount = tasks.length;
+
+  const today = new Date();
+  const todayCount = tasks.filter((t) => {
+    const taskDate = new Date(t.date);
+    return (
+      taskDate.getFullYear() === today.getFullYear() &&
+      taskDate.getMonth() === today.getMonth() &&
+      taskDate.getDate() === today.getDate()
+    );
+  }).length;
 
   const renderedTasks = tasks.map((t: Task) => {
     return (
@@ -13,10 +26,25 @@ export default async function Home() {
         className="flex justify-between items-center p-3 rounded hover:bg-gray-50"
       >
         <div className="flex flex-col">
-          <div className="text-lg text-gray-900">{t.task}</div>
+          <div className="text-md text-gray-900">{t.task}</div>
           <div className="text-blue-400 text-sm">{t.date.toLocaleDateString()}</div>
         </div>
-        <div className={`w-4 h-4 rounded-full ${t.completed ? 'bg-blue-500' : 'border border-gray-300'}`}></div>
+        <Star className={`w-4 h-4 rounded-full ${t.completed ? 'text-blue-500 fill-blue-500' : 'text-gray-300'}`}></Star>
+      </Link>
+    )
+  })
+
+  const completedTasks = tasks.filter((t) => t.completed)
+  const completedCount = completedTasks.length;
+  const renderedCompleted = completedTasks.map((t: Task) => {
+    return (
+      <Link
+        href={`/tasks/${t.id}`}
+        key={t.id}
+        className="flex items-center p-2 text-sm rounded hover:bg-gray-100"
+      >
+        <Check className="text-white bg-blue-500 rounded w-4 h-4" />
+        <span className="text-gray-900 ml-2">{t.task}</span>
       </Link>
     )
   })
@@ -24,7 +52,9 @@ export default async function Home() {
   return (
     <div className="flex flex-col md:flex-row w-full min-h-screen">
       <div className="md:w-1/5">
-        <Dashboard />
+        <Dashboard totalCount={totalCount}
+          completedCount={completedCount}
+          todayCount={todayCount} />
       </div>
 
       <div className="w-full md:w-3/5 p-4">
@@ -33,7 +63,7 @@ export default async function Home() {
             <h2 className="font-bold text-xl">My Day</h2>
             <p className="text-gray-500">May 2025</p>
           </div>
-          <Link href="/tasks/new" className="bg-green-100 rounded-xl p-3 text-blue-500 text-sm hover:bg-green-200 transition-colors">
+          <Link href="/tasks/new" className="bg-gray-100 rounded-xl p-3 text-blue-500 text-sm hover:bg-blue-200 transition-colors">
             + New Task
           </Link>
         </div>
@@ -41,7 +71,11 @@ export default async function Home() {
       </div>
 
       <div className="hidden md:block md:w-1/5 p-2">
-        <Calendar />
+        <CalendarPage />
+        <div>
+          <p className="font-semibold mt-3 mb-2">Completed Tasks</p>
+          <div className="space-y-1">{renderedCompleted}</div>
+        </div>
       </div>
     </div>
   );
--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -24,21 +24,25 @@ export default async function ShowTasksPage({params}: PageProps) {
 
     const deleteTaskAction = actions.deleteTask.bind(null, task.id)
     return (
-        <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded shadow">
-            <h1 className="text-xl font-bold mb-4">Task Details</h1>
-            <p>Task: {task.task}</p>
-            <p>Date: {new Date(task.date).toLocaleDateString()}</p>
-            <p>Completed: {task.completed ? "Yes" : "No"}</p>
+        <div>
+            <Link href={'/'} className="font-bold p-4">Home</Link>
 
-            <div className="flex justify-end">
-                <Link href={`/tasks/${task.id}/edit`} className="text-blue-600 hover:text-blue-800 mr-2">
-                    <Pencil className="w-5 h-5" />
-                </Link>
-                <form action={deleteTaskAction}>
-                    <button className="text-red-600 hover:text-red-800">
-                        <Trash2 className="w-5 h-5" />
-                    </button>
-                </form>
+            <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded shadow">
+                <h1 className="text-xl font-bold mb-4">Task Details</h1>
+                <p>Task: {task.task}</p>
+                <p>Date: {new Date(task.date).toLocaleDateString()}</p>
+                <p>Completed: {task.completed ? "Yes" : "No"}</p>
+
+                <div className="flex justify-end">
+                    <Link href={`/tasks/${task.id}/edit`} className="text-blue-600 hover:text-blue-800 mr-2">
+                        <Pencil className="w-5 h-5" />
+                    </Link>
+                    <form action={deleteTaskAction}>
+                        <button className="text-red-600 hover:text-red-800">
+                            <Trash2 className="w-5 h-5" />
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     )
